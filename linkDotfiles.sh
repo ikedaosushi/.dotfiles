d60bb21fd7afaaafaa959fa6315329d70738968f
ikedaosushi <ikeda.yutaro@gmail.com>
2020-03-21 20:46:38 +0900

    Add tigrc

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/.tigrc ~/.tigrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/config.fish ~/.config/fish/config.fish

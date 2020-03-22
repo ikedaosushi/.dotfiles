fa922649281ab19b4aee72ee42a4c9887a03bb37
ikedaosushi <ikeda.yutaro@gmail.com>
2020-03-22 23:35:40 +0900

    Tmp

--- config.fish
+++ config.fish
@@ -1,17 +1,3 @@
-# set -gx fish_theme agnoster
-
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-# function fish_user_key_bindings
-#   bind \cr peco_select_history # bind for peco history to ctrl + r
-#   bind \c] peco_select_ghq_repository # bind for peco change dir to ctrl + ]
-# end
-
-set -x LANG ja_JP.UTF-8
-
 # pyenv 
 set -x PYENV_ROOT $HOME/.pyenv
 set -x PATH $PYENV_ROOT/shims $PATH
@@ -33,7 +19,12 @@ eval (direnv hook fish)
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-
 alias mkdir='mkdir -p'
-
 alias g='git'
+
+# Fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -2,5 +2,5 @@
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.tigrc ~/.tigrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/fish ~/.config/fish
-ln -sf ~/.dotfiles/fisher ~/.config/fisher
+ln -sf ~/.dotfiles/fish ~/.config
+ln -sf ~/.dotfiles/fisher ~/.config
